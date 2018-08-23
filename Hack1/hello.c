ec64194e1a82c9ec0441b09db9d675e11fadfcf4
tlawrenson <42647279+tlawrenson@users.noreply.github.com>
2018-08-23 10:25:48 -0500

    initial commit

---
+++ Hack1/hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Tanner Lawrenson, Zavaire Ingram
+ * Date: 2018/8/21
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Tanner, Zavaire\n");
+
+  return 0;
+}
