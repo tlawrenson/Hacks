7103aa814eefefb3adda522d685937c970c19cf0
AmandaWhitlatch <Amandawhitlatch@gmail.com>
2018-08-23 19:51:37 -0500

    Added Name for Collaboration
    Added name

--- Hack1/hello.c
+++ Hack1/hello.c
@@ -1,5 +1,5 @@
 /**
- * Author: Tanner Lawrenson
+ * Author: Tanner Lawrenson, Amanda Whitlatch
  * Date: 2018/8/23
  *
  * A simple hello world program in C
@@ -10,7 +10,7 @@
 
 int main(int argc, char **argv) {
 
-  printf("Tanner Lawrenson, Computer Science\n");
+  printf(Tanner Lawrenson, Computer Science\n");
 
   return 0;
 }
